794ef949a3c79cee1d51a2a6bcc008fcd9984272
Kevin Maiorini <kmaiorini@brewinghub.com>
2011-08-14 19:38:36 -0400

    Added Help Page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   
   render_views
+  
+  before(:each) do
+    @base_title="Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +15,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title",:content => @base_title + " | Home")
     end
   end
 
@@ -22,7 +26,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",:content => @base_title + " | Contact")
     end
   end
   
@@ -33,7 +37,18 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title",:content => @base_title + " | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",:content => @base_title + " | Help")
     end
   end
 
