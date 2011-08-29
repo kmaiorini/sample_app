63fa2fb455e4ddac5221a3f8e779452c49f6cfcf
Kevin Maiorini <kmaiorini@brewinghub.com>
2011-08-29 10:53:36 -0400

    modified:   app/helpers/application_helper.rb 	modified:   app/views/layouts/_header.html.erb 	modified:   spec/controllers/users_controller_spec.rb 	modified:   spec/requests/layout_links_spec.rb 	new file:   webrat.log

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,9 +11,7 @@ describe UsersController do
     
     it "should have the right title" do
       get 'new'
-      response.should have_selector ("title", :content => "Sign up")
-    end
-    
+      response.should have_selector("title", :content => "Sign up")
+    end  
   end
-
 end
