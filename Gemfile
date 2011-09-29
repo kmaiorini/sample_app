b8bacf626eacadf52bd8605d758272b8c2862726
Kevin Maiorini <kmaiorini@brewinghub.com>
2011-09-29 16:14:14 -0400

    added user microposts

--- Gemfile
+++ Gemfile
@@ -7,17 +7,19 @@ gem 'rails', '3.0.1'
 
 gem 'sqlite3-ruby','1.2.5', :require => 'sqlite3'
 gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
 	gem 'rspec-rails', '2.0.1'
 	gem 'annotate'
 	gem 'faker', '0.3.1'
-	gem 'will_paginate', '3.0.pre2'
 end
 
 group :test do
 	gem 'rspec', '2.0.1'
 	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
+	gem 'spork', '0.8.4'
 	gem 'factory_girl_rails', '1.0'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   attr_accessible :name, :email, :password, :password_confirmation
   
+  has_many :microposts, :dependent => :destroy
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
@@ -35,6 +37,10 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
